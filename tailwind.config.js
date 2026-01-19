4ac95b7a9ccc0fa5ae8fce6cdef2c66aab5285f9
tadismukwindidza-cmyk <tadismukwindidza@gmail.com>
2026-01-19 14:36:51 +0200

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "tailwindcss";
+
+export default defineConfig({
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+});
