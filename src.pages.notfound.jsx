22e837c1a7e599f0774e5d2fa12ba734aa053889
tadismukwindidza-cmyk <tadismukwindidza@gmail.com>
2026-01-19 14:31:49 +0200

    src.pages.notfound.jsx

---
+++ src.pages.notfound.jsx
@@ -0,0 +1,23 @@
+import Navigation from "@/components/Navigation";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import ServicesSection from "@/components/ServicesSection";
+import PortfolioSection from "@/components/PortfolioSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navigation />
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <PortfolioSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
